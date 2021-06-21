61ce3970f6161e7b7aed54622bfc77f71c3298c1
amy-chinlai <amy@actionnetwork.org>
2021-06-21 15:20:55 -0400

    commit tree d692e81b54bbd97732db4921a39919d3ec51e35f

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
+def my_collect(collection)
+    counter = 0
+    collection_2 = []
+    while counter < collection.length
+        collection_2 << yield(collection[counter].split(" ")).first
+    end
+    collection_2
+end
 
-
+def my_collect(array)
+  counter = 0
+  languages = []
+  while counter < array.length
+    languages << yield(array[counter])
+    counter += 1
+  end
+  languages
+end
